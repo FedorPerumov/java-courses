d4610d3c1f16cc73e1f437c16b59522b86c6c3ea
FedorPerumov <Fedor.Perumov@gmail.com>
2021-04-27 21:39:35 +0300

    calculator

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,11 @@ public class Calculate{
 	// Class Calculate. It is Proba Java
 	public static void main(String[] arg){
 		System.out.println("Calculate");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum = " + sum);
+		int pow = first * second;
+		System.out.println("Pow = " + pow);
 	}
 }
